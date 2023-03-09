958cac1378cd6c76e3d103ed532120ab9666e0e7
Femi Ogunsakin <39163087+femilv@users.noreply.github.com>
2023-03-09 18:42:44 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,61 @@
+provider "azurerm" {
+  features {}
+}
+#Create Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "terraform-project"
+  location = "eastus"
+}
+
+
+#Create virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "terraform-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+#Create Subnet to hold the VM
+resource "azurerm_subnet" "sn" {
+  name                 = "VM"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+#Create vNIC for the VM and assign to the VM
+resource "azurerm_network_interface" "vmnic" {
+  name                = "terraformvm01-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+#Create the Virtual Machine 
+resource "azurerm_windows_virtual_machine" "terraformvm01" {
+  name                = "terraformvm01"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.vmnic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
